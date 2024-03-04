ca6a67d9f8f426e627f5ea2a85ec061951b3ebd9
Magda <magda.ahmed.osman21@gmail.com>
2024-03-04 13:38:22 +0200

    magda

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+class ShopController extends Controller
+{
+   
+    
+    public function index()
+    {
+        $product = Product::orderBy('created_at', 'DESC')->paginate(4);
+        return view('shop', compact('product'));
+    }
+    public function productDetails($slug)
+    {
+        $product = Product::where('slug', $slug)->first();    
+        return view('details', ['product' => $product]);
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ShopController;
 /*
-|--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
@@ -16,6 +16,8 @@ use App\Http\Controllers\AdminController;
 */
 
 Route::get('/', [AppController::class,'home']);
+Route::get('/shop', [ShopController::class,'index']);
+Route::get('/product/{slug}',[ShopController::class,'productDetails'])->name('shop.product.details');
 
 Route::middleware('auth')->group(function(){
     Route::get('/my-account', [UserController::class,'index'])->name('user.index');
